6b9e0b8e0fa69db1a8e56e4cbd21cf0a771eeedc
Leonardo Godinho <godinho.leo@gmail.com>
2020-03-20 17:45:28 -0300

    Service Criado

--- cliente-lembrete/src/app/app.module.ts
+++ cliente-lembrete/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorMsgComponent } from './compartilhado/error-msg/error-msg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorMsgComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- cliente-lembrete/src/environments/environment.ts
+++ cliente-lembrete/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  lembretesApiUrl: 'https://deviup.com.br:3001/api/lembrete'
 };
 
 /*
